677f8f27ec8e349b5e8804851af6780e7b6b5b80
Chieh Ning Wei <chiehningwei@Chiehteki-MacBook-Pro.local>
2020-08-25 17:45:48 +0800

    v1

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 
@@ -50,6 +40,26 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
+
+				data := url.Values{
+					"action":       {"CO2O"},
+					"message_type": {"line"},
+					"user_id":      {"5622512"},
+					"tel":          {"123456"},
+					"name":         {"gogo610"},
+					"birthday":     {"1010101"},
+				}
+				resp, err := http.PostForm("http://192.168.100.101:8000/610_is_good/user_info", data)
+
+				if err != nil {
+					panic(err)
+				}
+
+				var res interface{}
+
+				json.NewDecoder(resp.Body).Decode(&res)
+
+				fmt.Println(res)
 				quota, err := bot.GetMessageQuota().Do()
 				if err != nil {
 					log.Println("Quota err:", err)
