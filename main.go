d157e738822a874bf534d3665652a7b7fc93ef8b
Chieh Ning Wei <chiehningwei@Chiehteki-MacBook-Pro.local>
2020-08-26 14:58:30 +0800

    v4

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -42,25 +44,25 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				}
 
-				// data := url.Values{
-				// 	"action":       {"CO2O"},
-				// 	"message_type": {"line"},
-				// 	"user_id":      {"5622512"},
-				// 	"tel":          {"123456"},
-				// 	"name":         {"gogo610"},
-				// 	"birthday":     {"1010101"},
-				// }
-				// resp, err := http.PostForm("http://192.168.100.101:8000/610_is_good/user_info", data)
+				data := url.Values{
+					"action":       {"CO2O"},
+					"message_type": {"line"},
+					"user_id":      {"5622512"},
+					"tel":          {"123456"},
+					"name":         {"gogo610"},
+					"birthday":     {"1010101"},
+				}
+				resp, err := http.PostForm("http://192.168.100.101:8000/610_is_good/user_info", data)
 
-				// if err != nil {
-				// 	panic(err)
-				// }
+				if err != nil {
+					panic(err)
+				}
 
-				// var res interface{}
+				var res interface{}
 
-				// json.NewDecoder(resp.Body).Decode(&res)
+				json.NewDecoder(resp.Body).Decode(&res)
 
-				// fmt.Println(res)
+				fmt.Println(res)
 
 				// quota, err := bot.GetMessageQuota().Do()
 				// if err != nil {
