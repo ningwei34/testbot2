0acfbdb720c62f87798fe4bc930e5c0828c217db
Chieh Ning Wei <chiehningwei@Chiehteki-MacBook-Pro.local>
2020-08-27 16:57:00 +0800

    v13

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+"\n"+event.Source.UserID+"\n"+event.Postback.Data)).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+"\n"+event.Source.UserID)).Do(); err != nil {
 					log.Print(err)
 				}
 
